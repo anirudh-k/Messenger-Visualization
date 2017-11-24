5090e99bb7abba25d4337d64992dd8d60da25880
anirudh-k <anirudh.kaushik0@gmail.com>
2017-11-24 14:24:50 -0500

    added word count frequency

--- usage_calc.py
+++ usage_calc.py
@@ -4,23 +4,30 @@ import operator
 messages = json.load(open('messages.json'))['messages']
 
 users = {}
-count = 0.0
+message_count = 0.0
+word_count = 0.0
 
 for m in messages:
     user = m['sender']
+    text = m['text'].strip().split(' ')
     if user in users:
-        users[user] += 1
+        users[user] = (users[user][0] + 1, users[user][1] + len(text))
     else:
-        users[user] = 1
-    count += 1
+        users[user] = (1, len(text))
+    message_count += 1
+    word_count += len(text)
+
+print(message_count)
+print(word_count)
 
 for k in users:
-    users[k] = 100 * users[k] / count
+    users[k] = (100 * users[k][0] / message_count,
+                100 * users[k][1] / word_count)
 
 sorted_users = sorted(users.items(), key=operator.itemgetter(1), reverse=True)
 
 usage = open('usage.tsv', 'w')
-usage.write('%s %s\n' % ('User', 'Frequency'))
-for user, pct in sorted_users:
-    usage.write('%s %.5f%%\n' % (user, pct))
+usage.write('%s %s %s\n' % ('User', 'Message Frequency', 'Word Frequency'))
+for user, (msg_pct, word_pct) in sorted_users:
+    usage.write('%s %.5f%% %.5f%%\n' % (user, msg_pct, word_pct))
 usage.close()
